442e2cd57567e4d499cb72cee2b3f905c71d7942
Wendy <wendy89711@gmail.com>
2019-12-17 16:08:03 +0800

    按鈕變圖檔

--- app/src/main/java/com/wen/mapgame/MainActivity.java
+++ app/src/main/java/com/wen/mapgame/MainActivity.java
@@ -13,14 +13,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-     findViewById(R.id.button_left);
-     findViewById(R.id.button_down);
-     findViewById(R.id.button_right);
-     findViewById(R.id.button_up);
+        findViews();
+    }
+
+    private void findViews() {
+        findViewById(R.id.button_left).setOnClickListener(this);
+        findViewById(R.id.button_down).setOnClickListener(this);
+        findViewById(R.id.button_right).setOnClickListener(this);
+        findViewById(R.id.button_up).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Log.d("MainActivity","onClick");
+        switch (v.getId()) {
+            case R.id.button_left:
+                Log.d("MainActivity","onClick:LEFT");
+                break;
+            case R.id.button_down:
+                Log.d("MainActivity","onClick:DOWN");
+                break;
+            case R.id.button_right:
+                Log.d("MainActivity","onClick:RIGHT");
+                break;
+            case R.id.button_up:
+                Log.d("MainActivity","onClick:UP");
+                break;
+        }
     }
 }
