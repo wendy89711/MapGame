ba42910b5511dfd70aec28ad13b624999e306754
Wendy <wendy89711@gmail.com>
2019-12-17 15:13:06 +0800

    按鈕變成同一個ONCLICK

--- app/src/main/java/com/wen/mapgame/MainActivity.java
+++ app/src/main/java/com/wen/mapgame/MainActivity.java
@@ -3,12 +3,24 @@ package com.wen.mapgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+     findViewById(R.id.button_left);
+     findViewById(R.id.button_down);
+     findViewById(R.id.button_right);
+     findViewById(R.id.button_up);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Log.d("MainActivity","onClick");
     }
 }
